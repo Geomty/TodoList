0cb97c70b0d6c5ab44251ccf5403939550fcc935
Geomty <69478549+Geomty@users.noreply.github.com>
2024-07-16 22:16:09 -0400

    very basic layout

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import { Text, View, FlatList } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+import Constants from "expo-constants";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const statusBar = Constants.statusBarHeight;
+
   return (
-    <View className="p-8">
-      <FlatList data={[
-        "test",
-        "another test",
-        "yet another test",
-        "1",
-        "2",
-        "3"
-      ]} renderItem={item => <Text>{item.item}</Text>} />
-    </View>
+    <ScrollView>
+      <Text style={{ paddingTop: statusBar+10 }} className="pb-3 text-2xl text-center border-b border-black">To-do List</Text>
+      <View className="p-4 flex gap-4">
+        {[
+          "one",
+          "two",
+          "three",
+          "four",
+          "five",
+          "six",
+          "seven",
+          "eight",
+          "nine",
+          "ten",
+          "eleven",
+          "twelve",
+          "thirteen",
+          "fourteen",
+          "fifteen",
+          "sixteen",
+          "what",
+          "eighteen",
+          "nineteen",
+          "twenty"
+        ].map(item => { return (
+          <Text key={item} className="text-lg">{item}</Text>
+        )})}
+      </View>
+      <StatusBar backgroundColor="#00c5ff" />
+    </ScrollView>
   );
 }
