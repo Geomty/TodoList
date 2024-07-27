87938f37158507b76218a11ba0dc1a6b602a18bf
Geomty <69478549+Geomty@users.noreply.github.com>
2024-07-27 18:03:24 -0400

    more menu styling

--- app/(tabs)/completed.jsx
+++ app/(tabs)/completed.jsx
@@ -1,11 +1,14 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, useColorScheme } from "react-native";
 import { useState } from "react";
 import { useRoute } from "@react-navigation/native";
 import { Menu } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { MainLayout } from "../_layout";
 import * as storage from "../../scripts/storage";
+import * as colors from "../../constants/colors";
 
 export default function Completed() {
+  const colorScheme = useColorScheme();
   const route = useRoute();
   const [list, setList] = useState(route.params.list);
   const [curr, setCurr] = useState(route.params.list);
@@ -20,22 +23,36 @@ export default function Completed() {
       <View className="m-5 flex">
         {list.map((item, index) => {
           return (
-            <View key={index} className="mb-8 flex flex-row items-center">
+            <View key={index} className="mb-8 flex flex-row justify-between items-center">
               <Text className="text-xl flex-shrink text-black dark:text-white">{item}</Text>
               <Menu
                 visible={menu[index]}
-                onDismiss={() => {
-                  setMenu(Array(list.length).fill(false));
-                }}
-                anchor={<Pressable onPress={() => {
-                  setMenu([...Array(index).fill(false), true, ...Array(list.length-index-1)]);
-                }}><Text>a</Text></Pressable>}
-                theme={{ colors: { elevation: { level2: "white" } } }}
+                onDismiss={() => setMenu(Array(list.length).fill(false))}
+                anchor={
+                  <Pressable onPress={() => setMenu([...Array(index).fill(false), true, ...Array(list.length-index-1)])}>
+                    <MaterialCommunityIcons name="dots-vertical" color={colors.textColor(colorScheme)} size={24} />
+                  </Pressable>
+                }
+                theme={{ colors: { elevation: { level2: colorScheme == "dark" ? "black" : "white" } } }}
               >
-                <Menu.Item title="Delete" dense={true} onPress={async () => {
-                  setList(await storage.deleteItem("completed", index, list));
-                  setMenu(Array(list.length).fill(false));
-                }} />
+                <Menu.Item
+                  title="Restore"
+                  leadingIcon="undo"
+                  onPress={async () => {
+                    setList(await storage.deleteItem("completed", index, list));
+                    setMenu(Array(list.length).fill(false));
+                  }}
+                  theme={{ colors: { onSurfaceVariant: colors.textColor(colorScheme) } }}
+                />
+                <Menu.Item
+                  title="Delete"
+                  leadingIcon="trash-can-outline"
+                  onPress={async () => {
+                    setList(await storage.deleteItem("completed", index, list));
+                    setMenu(Array(list.length).fill(false));
+                  }}
+                  theme={{ colors: { onSurfaceVariant: colors.textColor(colorScheme) } }}
+                />
               </Menu>
             </View>
           )
