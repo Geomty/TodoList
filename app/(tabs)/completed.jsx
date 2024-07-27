2b83bd8b38370be9cfe3333a079907501f305140
Geomty <69478549+Geomty@users.noreply.github.com>
2024-07-26 21:13:30 -0400

    working menu

--- app/(tabs)/completed.jsx
+++ app/(tabs)/completed.jsx
@@ -4,27 +4,37 @@ import { Menu } from "react-native-paper";
 import { MainLayout } from "../_layout";
 import * as storage from "../../scripts/storage";
 
-export default function Home() {
-  const [menu, setMenu] = useState(false);
-
+export default function Completed() {
   return (
     <MainLayout store="completed" content={
       ({ colorScheme, list, setList }) => {
+        const [menu, setMenu] = useState([]);
+
         return (
-          <>
-            <View className="m-5 flex">
-              {list.map((item, index) => {
-                return (
-                  <View key={index} className="mb-8 flex flex-row items-center">
-                    <Text className="text-xl flex-shrink text-black dark:text-white">{item}</Text>
-                    <Menu visible={menu} onDismiss={() => setMenu(false)} anchor={<Pressable onPress={() => setMenu(true)}><Text>a</Text></Pressable>} theme={{ colors: { elevation: { level2: "white" } } }}>
-                      <Menu.Item title="Delete" dense={true} onPress={async () => setList(await storage.deleteItem("completed", index, list))} />
-                    </Menu>
-                  </View>
-                )
-              })}
-            </View>
-          </>
+          <View className="m-5 flex">
+            {list.map((item, index) => {
+              return (
+                <View key={index} className="mb-8 flex flex-row items-center">
+                  <Text className="text-xl flex-shrink text-black dark:text-white">{item}</Text>
+                  <Menu
+                    visible={menu[index]}
+                    onDismiss={() => {
+                      setMenu(Array(list.length).fill(false));
+                    }}
+                    anchor={<Pressable onPress={() => {
+                      setMenu([...Array(index).fill(false), true, ...Array(list.length-index-1)]);
+                    }}><Text>a</Text></Pressable>}
+                    theme={{ colors: { elevation: { level2: "white" } } }}
+                  >
+                    <Menu.Item title="Delete" dense={true} onPress={async () => {
+                      setList(await storage.deleteItem("completed", index, list));
+                      setMenu(Array(list.length).fill(false));
+                    }} />
+                  </Menu>
+                </View>
+              )
+            })}
+          </View>
         )
       }
     } />
