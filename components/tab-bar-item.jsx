319484834699386f5ad505acd8c39b912ed2e121
Geomty <69478549+Geomty@users.noreply.github.com>
2024-08-02 15:47:39 -0400

    another animation

---
+++ components/tab-bar-item.jsx
@@ -0,0 +1,46 @@
+import { Pressable } from "react-native";
+import Text from "./text";
+import { useColorScheme } from "nativewind";
+import Animated, { useSharedValue, withTiming, Easing } from "react-native-reanimated";
+import * as colors from "../constants/colors";
+import * as storage from "../scripts/storage";
+
+export default function TabBarItem({ state, descriptors, navigation, route, index }) {
+  const { colorScheme } = useColorScheme();
+  const borderRadius = useSharedValue(100);
+
+  const title = descriptors[route.key].options.title;
+  const Icon = descriptors[route.key].options.tabBarIcon;
+  const active = state.index == index;
+
+  const onPress = async () => {
+    borderRadius.value = 10;
+    borderRadius.value = withTiming(100, { duration: 750, easing: Easing.inOut(Easing.quad) });
+
+    const event = navigation.emit({
+      type: "tabPress",
+      target: route.key,
+      canPreventDefault: true,
+    });
+    
+    let list = [];
+    if (route.name == "index") {
+      list = await storage.readList("ongoing");
+    } else {
+      list = await storage.readList(route.name);
+    }
+
+    if (!active && !event.defaultPrevented) {
+      navigation.navigate(route.name, { list: list });
+    }
+  };
+
+  return (
+    <Pressable onPress={onPress} className="w-28 h-5/6">
+      <Animated.View style={{ borderRadius }} className={`w-full h-full flex justify-center items-center ${active && "bg-green-300 dark:bg-green-700"}`}>
+        <Icon color={colors.textColor(colorScheme)} size={24} />
+        <Text className="text-sm text-black dark:text-white">{title}</Text>
+      </Animated.View>
+    </Pressable>
+  )
+}
--- components/tab-bar.jsx
+++ components/tab-bar.jsx
@@ -1,45 +1,10 @@
-import { View, Pressable } from "react-native";
-import Text from "./text";
-import { useColorScheme } from "nativewind";
-import * as colors from "../constants/colors";
-import * as storage from "../scripts/storage";
+import { View } from "react-native";
+import TabBarItem from "./tab-bar-item";
 
 export default function TabBar({ state, descriptors, navigation }) {
-  const { colorScheme } = useColorScheme();
-
   return (
     <View className="h-16 w-full flex flex-row justify-around items-center bg-green-200 dark:bg-green-900">
-      {state.routes.map((route, index) => {
-        const title = descriptors[route.key].options.title;
-        const Icon = descriptors[route.key].options.tabBarIcon;
-        const active = state.index == index;
-
-        const onPress = async () => {
-          const event = navigation.emit({
-            type: "tabPress",
-            target: route.key,
-            canPreventDefault: true,
-          });
-          
-          let list = [];
-          if (route.name == "index") {
-            list = await storage.readList("ongoing");
-          } else {
-            list = await storage.readList(route.name);
-          }
-
-          if (!active && !event.defaultPrevented) {
-            navigation.navigate(route.name, { list: list });
-          }
-        };
-
-        return (
-          <Pressable key={route.key} onPress={onPress} className={`w-28 h-5/6 flex justify-center items-center rounded-full ${active && "bg-green-300 dark:bg-green-700"}`}>
-            <Icon color={colors.textColor(colorScheme)} size={24} />
-            <Text className="text-sm text-black dark:text-white">{title}</Text>
-          </Pressable>
-        )
-      })}
+      {state.routes.map((route, index) => <TabBarItem key={index} state={state} descriptors={descriptors} navigation={navigation} route={route} index={index} />)}
     </View>
   )
 }
